63a22f61fce943fb631df174a329551dccd37616
Endprodukt <o.carell@kabelmail.de>
2025-11-05 11:12:49 +0100

    konami

--- src/mame/konami/gticlub.cpp
+++ src/mame/konami/gticlub.cpp
@@ -302,8 +302,6 @@ protected:
 	optional_ioport_array<4> m_analog;
 	required_ioport_array<4> m_ports;
 	output_finder<3> m_pcb_digit;
-	output_finder<2> m_pcb_digit;
-	output_finder<1> m_pcb_output;
 	memory_view m_cg_view;
 
 	bool m_sound_irq_enabled = false;
